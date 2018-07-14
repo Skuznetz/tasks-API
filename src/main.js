dd520efb8c63a89c13e7ac2cf6618bba3d22ec6e
Skuznetz <s.kuznetzov@mail.ru>
2018-07-14 19:08:48 +0400

    сделаем роутинг и обернем в функцию

--- src/main.js
+++ src/main.js
@@ -6,10 +6,14 @@ import LoginPage from './components/loginPage.jsx';
 
 import App from './App.jsx';
 
-
-
+renderApp();
+function renderApp(){
     ReactDOM.render(
-        <App />,
+        <Router history={hashHistory}>
+            <Route path='/' component={App}>
+                <Route path='/login' component={LoginPage} />
+            </Route>
+        </Router>,
         document.getElementById('root')
     );
-
+}
