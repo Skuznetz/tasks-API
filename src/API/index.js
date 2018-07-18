f215fa7748d495f431c5c5bb4bc160dc2d7bfb62
Skuznetz <s.kuznetzov@mail.ru>
2018-07-18 18:51:01 +0400

    пишем авторизацию

--- src/API/index.js
+++ src/API/index.js
@@ -1,1 +1,20 @@
 const CLIENT_ID = '106602821630-t0sn6tbcmd224t2g4akekeogc9fs0fa0.apps.googleusercontent.com';
+const SCOPES = ['https://www.googleapis.com/auth/tasks', 'https://www.googleapis.com/auth/plus.me'];
+
+export default {
+    authorize(params) {
+        return new Promise((resolve, reject) => {
+            gapi.auth.authorize(
+                {
+                    'client_id': CLIENT_ID,
+                    'scope': SCOPES,
+                    'immediate': params.immediate,
+                    'cookie_policy': 'single_host_origin'
+                },
+                authResult => {
+                    console.log(authResult);
+                }
+            );
+        });
+    }
+}
