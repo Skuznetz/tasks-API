a4ad1b79b48eb63eae5095f5977b2af91c14845d
Skuznetz <s.kuznetzov@mail.ru>
2018-07-23 19:36:46 +0400

    добавим еще 2 константы

--- src/constants/AppConstants.js
+++ src/constants/AppConstants.js
@@ -2,5 +2,7 @@ import keyMirror from 'keymirror';
 
 export default keyMirror({
     SESSION_AUTHORIZE_SUCCESS    : null,
-    SESSION_AUTHORIZE_FAIL       : null
+    SESSION_AUTHORIZE_FAIL       : null,
+    TASK_LISTS_LOAD_SUCCESS      : null,
+    TASK_LISTS_LOAD_FAIL         : null
 });
