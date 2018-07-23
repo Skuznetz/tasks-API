992135eeb36adb6e9fe4f348290e8db649ae04b4
Skuznetz <s.kuznetzov@mail.ru>
2018-07-23 15:38:20 +0400

    создадим TasklistsPage.jsx

---
+++ src/components/TasklestsPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import List from 'material-ui/lib/lists/list';
+import ListItem from 'material-ui/lib/lists/list-item';
+import Divider from 'material-ui/lib/divider';
+import ListIcon from 'material-ui/lib/svg-icons/action/view-list';
+import HomeIcon from 'material-ui/lib/svg-icons/action/home';
+import ExitIcon from 'material-ui/lib/svg-icons/action/exit-to-app';
+import './TasklistsPage.less';
