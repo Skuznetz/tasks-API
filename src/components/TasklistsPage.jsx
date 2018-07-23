90c898efdc2eb2256e62639118cd4e88fcb36c2a
Skuznetz <s.kuznetzov@mail.ru>
2018-07-23 15:49:10 +0400

    пишем компонент

--- src/components/TasklistsPage.jsx
+++ src/components/TasklistsPage.jsx
@@ -6,3 +6,54 @@ import ListIcon from 'material-ui/lib/svg-icons/action/view-list';
 import HomeIcon from 'material-ui/lib/svg-icons/action/home';
 import ExitIcon from 'material-ui/lib/svg-icons/action/exit-to-app';
 import './TasklistsPage.less';
+
+
+
+const TasklistsPage = React.createClass({
+    contextTypes: {
+        router: React.PropTypes.object.isRequired
+    },
+
+    render() {
+        const { router } = this.context;
+
+        return (
+            <div className='TasklistsPage'>
+                <div className='TasklistsPage__menu'>
+                    <List className='TasklistsPage__list'>
+                        <h3 className='TasklistsPage__title'>Almost Google Tasks</h3>
+                        <Divider />
+                        <List className='TasklistsPage__list'>
+                            <ListItem
+                                leftIcon={<HomeIcon />}
+                                primaryText="Home"
+                                onClick={router.push.bind(null, `/lists`)}
+                            />
+                            <ListItem
+                                leftIcon={<ListIcon />}
+                                primaryText="About"
+                                onClick={router.push.bind(null, `/about`)}
+                            />
+                        </List>
+                        <Divider />
+                        <List className='TasklistsPage__list' subheader="Task Lists">
+                        </List>
+                        <Divider />
+                        <List className='TasklistsPage__list'>
+                            <ListItem
+                                leftIcon={<ExitIcon />}
+                                primaryText="Log out"
+                                onClick={this.handleLogOut}
+                            />
+                        </List>
+                    </List>
+                </div>
+                <div className='TasklistsPage__tasks'>
+                    {this.props.children}
+                </div>
+            </div>
+        );
+    },
+});
+
+export default TasklistsPage;
