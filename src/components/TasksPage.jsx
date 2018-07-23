b55096fa8a717c4586d699a95a92d17946e980ba
Skuznetz <s.kuznetzov@mail.ru>
2018-07-23 15:55:12 +0400

    напишем импорты

---
+++ src/components/TasksPage.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import IconButton from 'material-ui/lib/icon-button';
+import ContentAdd from 'material-ui/lib/svg-icons/content/add';
+import './TasksPage.less';
+import Task from './Task.jsx';
