b00e05105bec3bdf1085702d90e7d2992fba9c1d
Skuznetz <s.kuznetzov@mail.ru>
2018-07-23 16:02:04 +0400

    напишем компонент

--- src/components/TasksPage.jsx
+++ src/components/TasksPage.jsx
@@ -3,3 +3,19 @@ import IconButton from 'material-ui/lib/icon-button';
 import ContentAdd from 'material-ui/lib/svg-icons/content/add';
 import './TasksPage.less';
 import Task from './Task.jsx';
+
+const TasksPage = React.createClass({
+    render() {
+        return (
+            <div className='TasksPage'>
+                <div className='TasksPage__header'>
+                    <h2 className='TasksPage__title'>List name</h2>
+                    <div className='TasksPage__tools'>
+                        <IconButton onClick={this.handleAddTask}>
+                            <ContentAdd />
+                        </IconButton>
+                    </div>
+                </div>
+                <div className='TasksPage__tasks'>Tasks</div>
+ </div> );}});
+export default TasksPage;
