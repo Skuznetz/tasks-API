e7899aaae0f51637f3e54c86dbebdd6f9482301f
Skuznetz <s.kuznetzov@mail.ru>
2018-07-23 19:34:06 +0400

    создадим файл

---
+++ src/actions/TaskListsAction.js
@@ -0,0 +1,22 @@
+import AppDispatcher from '../dispatcher/AppDispatcher';
+import AppConstants from '../constants/AppConstants';
+
+import api from '../api';
+
+const TaskListsActions = {
+    loadTaskLists() {
+        api.listTaskLists()
+        .then(data => {
+            AppDispatcher.dispatch({
+                type: AppConstants.TASK_LISTS_LOAD_SUCCESS,
+                items: data.items
+            });
+        })
+        .catch(err => {
+            AppDispatcher.dispatch({
+                type: AppConstants.TASK_LISTS_LOAD_FAIL,
+                error: err
+            });
+        });
+    }
+};
