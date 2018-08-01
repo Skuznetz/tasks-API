4b2c9fee57d084bdc3c4a185517a96a675b85f5c
Skuznetz <s.kuznetzov@mail.ru>
2018-08-01 21:59:31 +0400

    напишем событие

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import Checkbox from 'material-ui/lib/checkbox';
 // import './Task.less';
 const Task = React.createClass({
+    handleCheck(){
+        this.props.onStatusChange({
+            isCompleted: !this.props.isCompleted
+        });
+    },
     render(){
         return (
             <div className='Task'>
             <Checkbox className='Task__checkbox'
-                      checked={this.props.isCompleted} />
+                      checked={this.props.isCompleted}
+                      onCheck={this.handleCheck} />
                 <div className='Task__text'>
                     <div className='Task__title'>{this.props.text}</div>
                     </div>
