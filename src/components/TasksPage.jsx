a4353166882cf0958f91a39c84e0b3129c058ef7
Skuznetz <s.kuznetzov@mail.ru>
2018-08-01 22:17:28 +0400

    импортируем компоненты

--- src/components/TasksPage.jsx
+++ src/components/TasksPage.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import TasksActions from '../actions/TasksActions';
+import TaskStore from '../stores/TaskStore';
+
 import IconButton from 'material-ui/lib/icon-button';
 import ContentAdd from 'material-ui/lib/svg-icons/content/add';
 import './TasksPage.less';
