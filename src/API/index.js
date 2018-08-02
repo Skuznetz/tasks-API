cd3e26a1e289451b217035033a5eb7eb661779d4
Skuznetz <s.kuznetzov@mail.ru>
2018-08-02 22:23:49 +0400

    метод updateTask

--- src/API/index.js
+++ src/API/index.js
@@ -38,6 +38,18 @@ export default {
         return new Promise((resolve, reject) => {
             request.execute(resp => resolve(resp));
         });
+    },
+      updateTask({ taskListId, taskId, ...params }) {
+        const request = gapi.client.tasks.tasks.update({
+            tasklist : taskListId,
+            task     : taskId,
+            id       : taskId,
+            ...params
+        });
+
+        return new Promise((resolve, reject) => {
+            request.execute(resp => resolve(resp));
+        });
     }
 
 }
