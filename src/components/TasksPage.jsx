e3b00ab6016d4b527baab5fe0d48cfbf5d667207
Skuznetz <s.kuznetzov@mail.ru>
2018-08-04 20:27:47 +0400

    импортируем taskCreateModal

--- src/components/TasksPage.jsx
+++ src/components/TasksPage.jsx
@@ -6,6 +6,9 @@ import IconButton from 'material-ui/lib/icon-button';
 import ContentAdd from 'material-ui/lib/svg-icons/content/add';
 import './TasksPage.less';
 import Task from './Task.jsx';
+import TaskCreateModal from './TaskCreateModal.jsx';
+
+
 
 
 
