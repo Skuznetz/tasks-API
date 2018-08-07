65b8a8859b9cca0397c9cb1f2856f2542a2c7c49
Skuznetz <s.kuznetzov@mail.ru>
2018-08-07 14:13:13 +0400

    импортируем элементы material-ui

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
 import Checkbox from 'material-ui/lib/checkbox';
+import ListItem from 'material-ui/lib/lists/list-item';
+import IconButton from 'material-ui/lib/icon-button';
+import IconMenu from 'material-ui/lib/menus/icon-menu';
+import MenuItem from 'material-ui/lib/menus/menu-item';
+import FlatButton from 'material-ui/lib/flat-button';
+import RaisedButton from 'material-ui/lib/raised-button';
+
+import DeleteIcon from 'material-ui/lib/svg-icons/action/delete';
+import MoreVertIcon from 'material-ui/lib/svg-icons/navigation/more-vert';
+
+
 // import './Task.less';
 const Task = React.createClass({
      getInitialState() {
