f1cbb6e8506c52f3f6808a3cb1d5874da50b0137
Skuznetz <s.kuznetzov@mail.ru>
2018-09-07 22:30:38 +0400

    перенесём методы в компонент

--- src/components/TasksPage.jsx
+++ src/components/TasksPage.jsx
@@ -25,6 +25,61 @@ const TasksPage = React.createClass({
         };
     },
 
+    componentWillMount() {
+        TasksActions.loadTasks(this.props.params.id);
+        // TaskListsActions.loadTaskList(this.props.params.id);
+    },
+
+    componentDidMount() {
+        TasksStore.addChangeListener(this._onChange);
+        // TaskListsStore.addChangeListener(this._onChange);
+    },
+
+    
+    componentWillReceiveProps(nextProps) {
+        if (this.props.params.id !== nextProps.params.id) {
+            TasksActions.loadTasks(nextProps.params.id);
+            // TaskListsActions.loadTaskList(nextProps.params.id);
+        }
+    },
+
+    componentWillUnmount() {
+        TasksStore.removeChangeListener(this._onChange);
+        // TaskListsStore.removeChangeListener(this._onChange);
+    },
+
+      handleStatusChange(taskId, { isCompleted }) {
+        TasksActions.updateTaskStatus({
+            taskListId: this.props.params.id,
+            taskId: taskId,
+            isCompleted: isCompleted
+        });
+    },
+
+    handleTaskUpdate(taskId, { text }) {
+        TasksActions.updateTask({
+            taskListId: this.props.params.id,
+            taskId: taskId,
+            text: text
+        });
+    },
+      handleAddTask() {
+        this.setState({ isCreatingTask : true });
+    },
+
+    handleClose() {
+       this.setState({ isCreatingTask : false });
+    },
+
+    handleTaskSubmit(task) {
+        const taskListId = this.props.params.id;
+
+        TasksActions.createTask({ taskListId, ...task });
+
+        this.setState({ isCreatingTask : false });
+    },
+
+
     render() {
         return (
             <div className='TasksPage'>
