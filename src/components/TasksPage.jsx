5434faef25dab945f7713c63a1be28b0c3b3a530
Skuznetz <s.kuznetzov@mail.ru>
2018-09-07 22:14:56 +0400

    запишем импорты

--- src/components/TasksPage.jsx
+++ src/components/TasksPage.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-// import TasksActions from '../actions/TasksActions';
-// import TasksStore from '../stores/TaskStore';
+import TasksActions from '../actions/TasksActions';
+import TasksStore from '../stores/TaskStore';
 
 import IconButton from 'material-ui/lib/icon-button';
 import ContentAdd from 'material-ui/lib/svg-icons/content/add';
 import './TasksPage.less';
 import Task from './Task.jsx';
-// import TaskCreateModal from './TaskCreateModal.jsx';
+import TaskCreateModal from './TaskCreateModal.jsx';
 
 const TasksPage = React.createClass({
     getInitialState() {
