888aedb955bc0df660492ec658609338a0cf1ab6
Skuznetz <s.kuznetzov@mail.ru>
2018-09-08 23:04:01 +0400

    исправим ошибку

--- src/components/loginPage.jsx
+++ src/components/loginPage.jsx
@@ -1,110 +1,77 @@
 import React from 'react';
 
-import TasksActions from '../actions/TasksActions';
-import TasksStore from '../stores/TaskStore';
+import SessionStore from '../stores/SessionStore';
+import SessionActions from '../actions/SessionActions';
 
-import IconButton from 'material-ui/lib/icon-button';
-import ContentAdd from 'material-ui/lib/svg-icons/content/add';
+import RaisedButton from 'material-ui/lib/raised-button';
 
-import Task from './Task.jsx';
-import TaskCreateModal from './TaskCreateModal.jsx';
-
-import './TasksPage.less';
+import './LoginPage.less';
 
 function getStateFromFlux() {
     return {
-        tasks: TasksStore.getTasks()
+        isLoggedIn: SessionStore.isLoggedIn()
     };
 }
 
-const TasksPage = React.createClass({
-    getInitialState() {
-        return {
-            ...getStateFromFlux(),
-            isCreatingTask: false
-        };
+const LoginPage = React.createClass({
+    contextTypes: {
+        router: React.PropTypes.object.isRequired
     },
 
-    componentWillMount() {
-        TasksActions.loadTasks(this.props.params.id);
+    getInitialState() {
+        return getStateFromFlux();
     },
 
     componentDidMount() {
-        TasksStore.addChangeListener(this._onChange);
+        SessionStore.addChangeListener(this._onChange);
+
+        if (this.state.isLoggedIn) {
+            this.redirectLoggedInUser();
+        }
     },
 
-    componentWillReceiveProps(nextProps) {
-        if (this.props.params.id !== nextProps.params.id) {
-            TasksActions.loadTasks(nextProps.params.id);
+    componentWillUpdate(nextProps, nextState) {
+        if (nextState.isLoggedIn) {
+            this.redirectLoggedInUser();
         }
     },
 
     componentWillUnmount() {
-        TasksStore.removeChangeListener(this._onChange);
+        SessionStore.removeChangeListener(this._onChange);
     },
 
-    handleStatusChange(taskId, { isCompleted }) {
-        TasksActions.updateTaskStatus({
-            taskListId: this.props.params.id,
-            taskId: taskId,
-            isCompleted: isCompleted
-        });
+    handleLogIn() {
+        SessionActions.authorize();
     },
 
-    handleTaskUpdate(taskId, { text }) {
-        TasksActions.updateTask({
-            taskListId: this.props.params.id,
-            taskId: taskId,
-            text: text
-        });
-    },
+    redirectLoggedInUser() {
+        const { location } = this.props
 
-    handleAddTask() {
-        this.setState({ isCreatingTask : true });
-    },
-
-    handleClose() {
-        this.setState({ isCreatingTask : false });
-    },
-
-    handleTaskSubmit(task) {
-        const taskListId = this.props.params.id;
-
-        TasksActions.createTask({ taskListId, ...task });
-
-        this.setState({ isCreatingTask : false });
+        if (location.state && location.state.nextPathname) {
+            this.context.router.replace(location.state.nextPathname);
+        } else {
+            this.context.router.replace('/lists');
+        }
     },
 
     render() {
         return (
-            <div className='TasksPage'>
-                <div className='TasksPage__header'>
-                    <h2 className='TasksPage__title'>List name</h2>
-                    <div className='TasksPage__tools'>
-                        <IconButton onClick={this.handleAddTask}>
-                            <ContentAdd />
-                        </IconButton>
+            <div className='LoginPage'>
+                <div className='LoginPage__banner'>
+                    <div className='LoginPage__text'>
+                        <h1>Almost Google tasks</h1>
+                        <p>Organise your life!</p>
+                        <RaisedButton
+                            className='login-button'
+                            label='Log in with Google'
+                            onClick={this.handleLogIn}
+                        />
                     </div>
+                    <img
+                        src='/img/desk.png'
+                        className='LoginPage__image'
+                    />
                 </div>
-
-                <div className='TasksPage__tasks'>
-                    {
-                        this.state.tasks.map(task =>
-                            <Task
-                                key={task.id}
-                                text={task.text}
-                                isCompleted={task.isCompleted}
-                                onStatusChange={this.handleStatusChange.bind(null, task.id)}
-                                onUpdate={this.handleTaskUpdate.bind(null, task.id)}
-                            />
-                        )
-                    }
-                </div>
-                <TaskCreateModal
-                    isOpen={this.state.isCreatingTask}
-                    onSubmit={this.handleTaskSubmit}
-                    onClose={this.handleClose}
-                />
             </div>
         );
     },
@@ -114,4 +81,4 @@ const TasksPage = React.createClass({
     }
 });
 
-export default TasksPage;
+export default LoginPage
